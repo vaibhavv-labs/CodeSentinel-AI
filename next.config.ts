a94961d09d4cfd2db16a94e4ac72974298d0d8ca
Vaibhav Bhoyate <vaibhavbhoyate478@gmail.com>
2026-05-20 13:32:17 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const configDir = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  turbopack: {
-    root: configDir,
-  },
   images: {
     remotePatterns: [
       {
